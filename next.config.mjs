e8da9797084e5f4e769f65e9587cd6ee8176d660
ABSphreak <absphreak@outlook.com>
2024-08-10 21:25:03 +0530

    feat: add hostname to the config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      { hostname: "ui.aceternity.com" },
-      { hostname: "sprintsocial.ai" },
-      { hostname: "cdn.youprotab.com" },
-      { hostname: "getaresu.me" },
-    ],
-  },
+	images: {
+		remotePatterns: [
+			{ hostname: 'ui.aceternity.com' },
+			{ hostname: 'sprintsocial.ai' },
+			{ hostname: 'cdn.youprotab.com' },
+			{ hostname: 'getaresu.me' },
+			{ hostname: 'fyg.app' },
+		],
+	},
 };
 
 export default nextConfig;
